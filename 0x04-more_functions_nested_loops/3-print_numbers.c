52d934f9956e75c7f04724469af626599ee95fcb
Onah Okechukwu Linus <oklinusonah@gmail.com>
2022-09-15 09:53:59 -0700

    Task 3. The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
 * print_numbers - function that prints the numbers, from 0 to 9.
@@ -6,8 +6,9 @@
 
 void print_numbers(void)
 {
-	char c;
+	int c;
 
 	for (c = '0'; c <= '9'; c++)
 	_putchar(c);
 	_putchar('\n');
+}
