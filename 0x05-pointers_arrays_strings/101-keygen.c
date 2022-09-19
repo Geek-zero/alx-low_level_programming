80d6b16f99e4e88c493dcda09e41be8bcf5b23e9
Onah Okechukwu Linus <oklinusonah@gmail.com>
2022-09-19 09:41:55 -0700

    Task 11. Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * program generates random valid passwords
+ * @s: Password
+ * Return (0)
+ */
+
+int main ( int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Usage: %s password", argv[0]);
+	}
+	else
+	{
+		if (condition to check for the password)
+			puts("Wrong password");
+		else
+			prints("Tada Congrats!");
+	}
+}
